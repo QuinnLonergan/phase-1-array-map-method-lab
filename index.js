c0f8cce62c36c5c73c1fe70a74df8825462a5d5b
Quinn Lonergan <qui99lo@gmail.com>
2021-06-30 18:26:52 -0400

    commit tree 7a6ba09c5f65a6fcacb6727817d358085404a1e3

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    const seperate = tutorial.split(' ');
+    const capitalized = seperate.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+  });
+  const final = capitalized.join(' ');
+  return final
+  });
 }
+
+
